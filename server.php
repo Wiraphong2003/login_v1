815c715ed45f447ec4b7a5ec5d2da2b82471cd46
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-01-29 02:12:04 +0700

    v1

---
+++ server.php
@@ -0,0 +1,13 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->setBasePath('/login');
+
+require __DIR__ . '/welcome.php';
+
+$app->run();
